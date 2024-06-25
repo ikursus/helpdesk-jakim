6f7f88baa1c9bf27ffafd029a99fed5182ab1c2b
Amirol Zolkifli <sitehandy@gmail.com>
2024-06-25 11:22:16 +0800

    Passing data to view tickets

--- resources/views/template-tickets/senarai-tickets.php
+++ resources/views/template-tickets/senarai-tickets.php
@@ -1,4 +1,48 @@
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+
+
 <h1>Senarai Ticket</h1>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>TITLE</th>
+            <th>PENGIRIM</th>
+            <th>EMAIL</th>
+        </tr>
+    </thead>
+
+    <tbody>
+
+        <?php foreach( $senaraiTickets as $ticket ): ?>
+            <tr>
+                <td><?php echo $ticket['id']; ?></td>
+                <td><?php echo $ticket['title']; ?></td>
+                <td><?php echo $ticket['submitter_name']; ?></td>
+                <td><?php echo $ticket['submitter_email']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+
+    </tbody>
+</table>
+
 
 <ul>
     <li><a href="<?php echo route('ticket.semak'); ?>">Semak Tiket</a></li>
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,22 @@ Route::get('/', function() {
 
 Route::get('/ticket', function () {
 
-    return view('template-tickets.senarai-tickets');
+    $senaraiTickets = [
+        ['id' => 1, 'title' => 'Ticket 1', 'submitter_name' => 'Ali', 'submitter_email' => 'ali@gmail.com'],
+        ['id' => 2, 'title' => 'Ticket 2', 'submitter_name' => 'Abu', 'submitter_email' => 'abu@gmail.com'],
+        ['id' => 3, 'title' => 'Ticket 3', 'submitter_name' => 'Siti', 'submitter_email' => 'siti@gmail.com'],
+        ['id' => 4, 'title' => 'Ticket 4', 'submitter_name' => 'Upin', 'submitter_email' => 'upin@gmail.com'],
+        ['id' => 5, 'title' => 'Ticket 5', 'submitter_name' => 'Ipin', 'submitter_email' => 'ipin@gmail.com'],
+    ];
+
+    // Cara 1 attach/passing data ke view
+    // return view('template-tickets.senarai-tickets')->with('senaraiTickets', $senaraiTickets);
+
+    // Cara 2 attach/passing data ke view
+    // return view('template-tickets.senarai-tickets', ['senaraiTickets' => $senaraiTickets]);
+
+    // Cara 3 attach/passing data ke view
+    return view('template-tickets.senarai-tickets', compact('senaraiTickets'));
 
 })->name('ticket.list');
 
